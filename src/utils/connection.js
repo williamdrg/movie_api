3a5b29050cf4e2a2e0bc999da88d97810d761d7e
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-08-06 08:24:35 -0500

    actualización de archivos

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false })
+const sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false}}
+ }
+  
+)
 
 module.exports = sequelize;
